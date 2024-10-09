135d3ea943f7f9110fb65dd9b229daf5c863c799
Kratae Chadaporn <kratae@Kratae.local>
2024-10-09 22:30:53 +0700

    run test

--- dist/test.js
+++ dist/test.js
@@ -11,20 +11,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
 const unit_test = () => __awaiter(void 0, void 0, void 0, function* () {
-    if (Utils_1.Utils.add(1, 3) === 23445) {
-        console.log(1);
+    //     if (Utils.add(1, 3) === 23445) {
+    //         console.log(1)
+    //     } else {
+    //         console.log(0)
+    //         return;
+    //     }
+    // Test 2
+    const result2 = Utils_1.Utils.countChar("cadsadt");
+    console.log(result2);
+    if (result2 === 5) {
+        console.log(0);
+        return 0;
     }
     else {
-        console.log(0);
-        return;
+        console.log(1);
+        return 1;
     }
-    // // Test 2
-    // const result2 = Utils.countChar("cadsadt");
-    // console.log(result2)
-    // if (result2 === 5) {
-    //     console.log(0);
-    // } else {
-    //     console.log(1);
-    // }
 });
 exports.default = unit_test;
--- src/test.ts
+++ src/test.ts
@@ -2,25 +2,27 @@
 import { Utils } from './Utils';
 
 
- const unit_test = async () => {
-    if (Utils.add(1, 3) === 23445) {
+  const unit_test = async () => {
+//     if (Utils.add(1, 3) === 23445) {
 
-        console.log(1)
+//         console.log(1)
+//     } else {
+//         console.log(0)
+//         return;
+//     }
+
+
+    // Test 2
+    const result2 = Utils.countChar("cadsadt");
+    console.log(result2)
+    if (result2 === 5) {
+        
+        console.log(0);
+        return 0;
     } else {
-        console.log(0)
-        return;
+        console.log(1);
+        return 1;
     }
-
-
-    // // Test 2
-    // const result2 = Utils.countChar("cadsadt");
-    // console.log(result2)
-    // if (result2 === 5) {
-       
-    //     console.log(0);
-    // } else {
-    //     console.log(1);
-    // }
 }
 
 
